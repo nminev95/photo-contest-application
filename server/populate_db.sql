9f92f7f449d8c9171349aeebe78fcd2114243a7a
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-11-03 19:03:29 +0200

    populate db with initial data sql

---
+++ server/populate_db.sql
@@ -0,0 +1,19 @@
+INSERT INTO `photo_contest_db`.`roles` (`type`) VALUES ('Organizer');
+INSERT INTO `photo_contest_db`.`roles` (`type`) VALUES ('Photo Junkie');
+
+INSERT INTO `photo_contest_db`.`ranks` (`type`) VALUES ('Junkie');
+INSERT INTO `photo_contest_db`.`ranks` (`type`) VALUES ('Enthusiast');
+INSERT INTO `photo_contest_db`.`ranks` (`type`) VALUES ('Master');
+INSERT INTO `photo_contest_db`.`ranks` (`type`) VALUES ('Wise and Benevolent Photo Dictator');
+
+INSERT INTO `photo_contest_db`.`contest_phases` (`type`) VALUES ('Phase I');
+INSERT INTO `photo_contest_db`.`contest_phases` (`type`) VALUES ('Phase II');
+INSERT INTO `photo_contest_db`.`contest_phases` (`type`) VALUES ('Finished');
+
+INSERT INTO `photo_contest_db`.`content_restrictions` (`type`) VALUES ('Open');
+INSERT INTO `photo_contest_db`.`content_restrictions` (`type`) VALUES ('Invitational');
+
+
+
+
+
---
+++ server/src/config.js
@@ -0,0 +1,16 @@
+export const DB_CONFIG = {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'MarVel147',
+    database: 'mydb',
+};
+
+export const PORT = 4000;
+
+export const PRIVATE_KEY = 'sekreten_chasten_klu4';
+
+// 60 mins * 60 secs
+export const TOKEN_LIFETIME = 60 * 60; 
+
+export const DEFAULT_USER_ROLE = 'user';
