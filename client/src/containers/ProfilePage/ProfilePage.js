47e98bf72e27d998a9f73c4c4dc7a8ccf6c13a99
Mariya Velikova <m.velikovaa@gmail.com>
2020-11-05 19:06:48 +0200

    Profile

--- client/src/containers/ProfilePage/ProfilePage.js
+++ client/src/containers/ProfilePage/ProfilePage.js
@@ -1,20 +1,40 @@
 
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import { positions } from '@material-ui/system';
+import Avatar from '@material-ui/core/Avatar';
+import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
+import PhotoCameraOutlinedIcon from '@material-ui/icons/PhotoCameraOutlined';
+
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 0.50,
-        
+    icon: {
+        marginRight: theme.spacing(2),
     },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
+    upperContent: {
+        backgroundColor: "#e0e0e0",
+        padding: theme.spacing(15, 0, 6),
+
+    },
+    middleContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(10, 0, 6),
+        borderRadius: 16,
+        borders: 1
+    },
+    bottomContent: {
+        backgroundColor: "#e0e0e0",
+        padding: theme.spacing(8, 0, 6),
+    },
+    Buttons: {
+        marginTop: theme.spacing(4),
+    },
+    Icons: {
+        marginTop: theme.spacing(4),
     },
 }));
 
@@ -24,20 +44,106 @@ const ProfilePage = () => {
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}></Paper>
-                </Grid>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}></Paper>
-                </Grid>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}></Paper>
-                </Grid>
-               
-            </Grid>
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <main >
+                <div className={classes.upperContent} maxWidth="sm">
+                    <Container maxWidth="md">
+                        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                        </Typography>
+                        <div className={classes.Buttons}>
+                            <Grid container spacing={2} justify="center">
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+                <div className={classes.middleContent}>
+                    <Container maxWidth="md">
+
+                        <Grid container spacing={1} justify="center" >
+                            <Avatar className={classes.large}></Avatar>
+                        </Grid>
+                        <Typography component="h6" variant="h4" align="center" color="textPrimary" gutterBottom>
+                            Maria Velikova
+                        </Typography>
+                        <div>
+                            <Grid container spacing={2} justify="center" >
+                                <Grid item>
+                                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                                        Rank: Newbie
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                                        Points: 2
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </div>
+                        <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                            Let other people know more about you...
+                        </Typography>
+                        <div className={classes.Buttons}>
+                            <Grid container spacing={2} justify="center" >
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Awards
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Contest history
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+                <div className={classes.bottomContent}>
+                            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                                Joined: Nov 5 2020
+                            </Typography>
+                    <Container maxWidth="md">
+                        
+                        <div>
+                            <Grid container spacing={2} justify="center" >
+                                <Grid item>
+                                    <ImageOutlinedIcon style={{ fontSize: 60 }}></ImageOutlinedIcon>
+                                </Grid>
+                                <Grid item>
+                                    <PhotoCameraOutlinedIcon style={{ fontSize: 100 }}></PhotoCameraOutlinedIcon>
+                                </Grid>
+                                <Grid item>
+                                    <ImageOutlinedIcon style={{ fontSize: 60 }}></ImageOutlinedIcon>
+                                </Grid>
+                            </Grid>
+                        </div>
+                        <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                            Your profile is a bit empty!
+                        </Typography>
+                        <Typography variant="h7" align="center" color="textSecondary" paragraph>
+                            This is the place to show your bestwork. Take part in many contests, win a lot of awards and be proud of You!
+                        </Typography>
+                        <div className={classes.Buttons}>
+                            <Grid container spacing={2} justify="center" >
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        Update your profile
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+            </main>
+        </React.Fragment>
     );
 }
 
