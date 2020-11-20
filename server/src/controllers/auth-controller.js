a5483a43f82192d0d089a191b782635a3b7f144c
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-11-20 16:08:27 +0200

    backend auth code refactor - user controller => auth controller

--- server/src/controllers/auth-controller.js
+++ server/src/controllers/auth-controller.js
@@ -1,11 +1,58 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { createAccessToken } from '../auth/create-token.js';
+import { createAccessToken, createRefreshToken } from '../auth/create-token.js';
 import { REFRESH_TOKEN_SECRET_KEY } from '../constants/config.js';
+import usersData from '../data/users-data.js';
+import usersService from '../services/users-service.js';
+import { createUserSchema } from '../validations/schemas/create-user-schema.js';
+import { createValidator } from '../validations/validator-middleware.js';
+import * as ERRORS from '../constants/service-errors.js';
 
 const authController = express.Router();
 
 authController
+    .post('/register',
+        createValidator(createUserSchema),
+        async (req, res) => {
+
+            const userData = req.body;
+            const { error, user } = await usersService.createUser(usersData)(userData);
+
+            if (error === ERRORS.DUPLICATE_RECORD) {
+                res.status(409).send({ message: 'Username is not available' });
+            } else if (error === ERRORS.NO_MATCH) {
+                res.status(403).send({ message: 'Passwords don\'t match' });
+            } else {
+                res.status(201).send(user);
+            }
+        },
+    )
+    .post('/login',
+        async (req, res) => {
+
+            const { username, password } = req.body;
+            const { error, user } = await usersService.signInUser(usersData)(username, password);
+
+            if (error === ERRORS.INVALID_SIGNIN) {
+                res.status(401).send({
+                    message: 'Invalid username/password',
+                });
+            } else {
+                const payload = {
+                    sub: user.id,
+                    username: user.username,
+                    role: user.role,
+                };
+                const accessToken = createAccessToken(payload);
+                const refreshToken = createRefreshToken(payload);
+
+                res.status(200).send({
+                    accessToken: accessToken,
+                    refreshToken: refreshToken,
+                });
+            }
+        },
+    )
     .post('/token',
         async (req, res) => {
             const refreshToken = req.body.refreshToken;
@@ -14,7 +61,7 @@ authController
                 res.status(401).json({ message: 'No token found!' });
             }
             jwt.verify(refreshToken, REFRESH_TOKEN_SECRET_KEY, (err, user) => {
-           
+
                 if (err) {
                     res.status(403).json({ message: 'Invalid token!' });
                 }
--- server/src/controllers/users-controller.js
+++ server/src/controllers/users-controller.js
@@ -1,57 +1,12 @@
 import express from 'express';
-import { createAccessToken, createRefreshToken } from '../auth/create-token.js';
 import * as ERRORS from '../constants/service-errors.js';
 import usersData from '../data/users-data.js';
 import usersService from '../services/users-service.js';
-import { createUserSchema } from '../validations/schemas/create-user-schema.js';
-import { createValidator } from '../validations/validator-middleware.js';
 import { authMiddleware } from '../auth/auth-middleware.js';
 
 const usersController = express.Router();
 
 usersController
-    .post('/register',
-        createValidator(createUserSchema),
-        async (req, res) => {
-
-            const userData = req.body;
-            const { error, user } = await usersService.createUser(usersData)(userData);
-
-            if (error === ERRORS.DUPLICATE_RECORD) {
-                res.status(409).send({ message: 'Username is not available' });
-            } else if (error === ERRORS.NO_MATCH) {
-                res.status(403).send({ message: 'Passwords don\'t match' });
-            } else {
-                res.status(201).send(user);
-            }
-        },
-    )
-    .post('/login',
-        async (req, res) => {
-
-            const { username, password } = req.body;
-            const { error, user } = await usersService.signInUser(usersData)(username, password);
-
-            if (error === ERRORS.INVALID_SIGNIN) {
-                res.status(401).send({
-                    message: 'Invalid username/password',
-                });
-            } else {
-                const payload = {
-                    sub: user.id,
-                    username: user.username,
-                    role: user.role,
-                };
-                const accessToken = createAccessToken(payload);
-                const refreshToken = createRefreshToken(payload);
-                
-                res.status(200).send({
-                    accessToken: accessToken,
-                    refreshToken: refreshToken,
-                });
-            }
-        },
-    )
     .get('/:id/profile',
         authMiddleware,
         async (req, res) => {
