df6a856dc1d9c5bd025d377aef4a1a34a2c80af1
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-11-21 19:33:10 +0200

    axios interceptor response handle - generate and set new token

--- client/src/requests/axios.js
+++ client/src/requests/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { BASE_URL } from '../constants/constants';
+import userEndpoints from './user-requests';
+import decode from 'jwt-decode';
+import swal from 'sweetalert';
 
 // axios.interceptors.response.use((response) => {
 //     return response
@@ -7,7 +10,7 @@ import { BASE_URL } from '../constants/constants';
 //     const originalRequest = error.config;
 
 //     if (error.response.status === 403) {
-    
+
 //     return Promise.reject(error);
 // }
 
@@ -42,16 +45,52 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
     config => {
         const token = localStorage.getItem('accessToken');
-        console.log(token)
         if (token) {
-            config.headers['Authorization'] = 'Bearer ' + token;
+            config.headers = {
+                Authorization: {
+                    toString() {
+                        return `Bearer ${localStorage.getItem('accessToken')}`;
+                    }
+                }
+            }
         }
-        // config.headers['Content-Type'] = 'application/json';
         return config;
     },
     error => {
         Promise.reject(error)
-    });
+    }
+);
 
+axiosInstance.interceptors.response.use((response) => {
+    return response
+}, async function (error) {
+    console.log(error)
+    const originalRequest = error.config;
+    console.log(originalRequest)
+    if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        const oldToken = localStorage.getItem('accessToken');
+        const user = decode(oldToken);
+        return axiosInstance.post(userEndpoints.createNewToken, { id: user.sub })
+            .catch((error) => {
+                if (error.response.status > 400) {
+                    swal({
+                        title: "Oops!",
+                        text: "An unexpected error occured. Please try again.",
+                        icon: "error",
+                        button: "Ok"
+                    })
+                }
+            }).then((response) => {
+                const newToken = response.data
+                console.log(newToken);
+                localStorage.setItem('accessToken', newToken);
+                axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`.toString();
+                originalRequest.headers['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`.toString();
+                return axiosInstance(originalRequest);
+            })
+    }
+    return Promise.reject(error);
+});
 
 export default axiosInstance;
